0702a10da8ffea7db83b2b09b289b459d923eeb2
beata <beata_sadek@o2.pl>
2020-04-07 20:00:46 +0200

    zajecia 3 - zadanie 3

---
+++ src/zajecia3/zadanie3/MathClass.java
@@ -0,0 +1,19 @@
+package zajecia3.zadanie3;
+
+public class MathClass {
+    public static boolean isEven(int a) {
+        return a % 2 == 0;
+    }
+
+    public static boolean isOdd(int a) {
+        return a % 2 == 1;
+    }
+
+    public static double circleField(double r) {
+        return Math.PI * r * r;
+    }
+
+    public static long power(int a) {
+        return a * a;
+    }
+}
---
+++ src/zajecia3/zadanie3/MathTest.java
@@ -0,0 +1,10 @@
+package zajecia3.zadanie3;
+
+public class MathTest {
+    public static void main(String[] args) {
+        System.out.println(MathClass.isEven(1290898873));
+        System.out.println(MathClass.isOdd(1290898873));
+        System.out.println(MathClass.circleField(1));
+        System.out.println(MathClass.power(7));
+    }
+}
