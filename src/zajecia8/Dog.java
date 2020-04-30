cfc48640d7befda7db87017f4be93ad644852945
beata <beata_sadek@o2.pl>
2020-04-30 13:52:14 +0200

    zajecia 8 - Tablica z unikalnymi obiektami

---
+++ src/zajecia8/Dog.java
@@ -0,0 +1,19 @@
+package zajecia8;
+
+public class Dog {
+    private final String name;
+    private final int age;
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Dog)) return false;
+        Dog dog = (Dog) o;
+        return age == dog.age && name.equalsIgnoreCase(dog.name);
+    }
+}
---
+++ src/zajecia8/Test.java
@@ -0,0 +1,39 @@
+package zajecia8;
+
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Dog[] dogTab = new Dog[3];
+        for (int i = 0; i < dogTab.length; ) {
+            Dog dog = getDog();
+            if (checkIfDogNotExists(dogTab, dog, i)) {
+                dogTab[i] = dog;
+                System.out.println((i + 1) + ". pies dodany");
+                i++;
+            }
+        }
+        System.out.println("Wszystkie psy dodane");
+    }
+
+    public static Dog getDog() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj imie psa: ");
+        String name = scanner.nextLine();
+        System.out.println("Podaj wiek psa: ");
+        int age = scanner.nextInt();
+        return new Dog(name, age);
+    }
+
+    public static boolean checkIfDogNotExists(Dog[] tab, Dog dog, int j) {
+        for (int i = 0; i < j; i++) {
+            if (tab[i].equals(dog)) {
+                System.out.println("Ten pies juz jest");
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+
