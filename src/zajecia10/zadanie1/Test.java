740fc7552d071b6159a679bf79ec251194ed3ebe
beata <beata_sadek@o2.pl>
2020-05-10 15:38:28 +0200

    zajecia 10 - zadanie 1 fix 1

--- src/zajecia10/zadanie1/Test.java
+++ src/zajecia10/zadanie1/Test.java
@@ -6,32 +6,23 @@ public class Test {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        Person person = null;
-        String firstName;
-        String lastName;
-        int age;
-        String pesel;
-
         System.out.print("Podaj imie: ");
-        firstName = scanner.nextLine();
+        String firstName = scanner.nextLine();
         System.out.print("Podaj nazwisko: ");
-        lastName = scanner.nextLine();
+        String lastName = scanner.nextLine();
         System.out.print("Podaj wiek: ");
-        age = scanner.nextInt();
+        int age = scanner.nextInt();
         System.out.print("Podaj pesel: ");
-        pesel = scanner.next();
+        String pesel = scanner.next();
 
         try {
-            person = new Person(firstName, lastName, age, pesel);
+            Person person = new Person(firstName, lastName, age, pesel);
+            System.out.println(person.toString());
+
         } catch (NameUndefinedException | IncorrectAgeException e) {
             System.err.println(e.getMessage());
         }
 
-        try {
-            System.out.println(person.toString());
-        } catch (NullPointerException e) {
-            System.err.println("Person has not been created");
-        }
 
     }
 
