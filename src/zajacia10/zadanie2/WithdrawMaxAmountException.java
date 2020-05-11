95bf7e03bacc54e2e3af4af82fc3f214934f136d
beata <beata_sadek@o2.pl>
2020-05-11 10:27:52 +0200

    zajecia 10 - zadanie 2 fix 1

--- src/zajacia10/zadanie2/WithdrawMaxAmountException.java
+++ src/zajacia10/zadanie2/WithdrawMaxAmountException.java
@@ -2,6 +2,6 @@ package zajacia10.zadanie2;
 
 public class WithdrawMaxAmountException extends RuntimeException {
     public WithdrawMaxAmountException(double credit, double max) {
-        System.err.println("Credit amount " + credit + " is above the limit set to " + max + ".");
+        super("Credit amount " + credit + " is above the limit set to " + max + ".");
     }
 }
