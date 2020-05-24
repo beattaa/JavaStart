2b11274c2cda43c916b220a019fd92a44821190f
beata <beata_sadek@o2.pl>
2020-05-24 20:00:47 +0200

    zajecia 14 - zadanie 1: Palindrom tablica

---
+++ src/zajecia14/zadanie1/Check.java
@@ -0,0 +1,12 @@
+package zajecia14.zadanie1;
+
+public class Check {
+    public static boolean isPalindrome(int[] array) {
+        for (int i = 0; i < array.length / 2; i++) {
+            if (array[i] != array[array.length - 1 - i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
