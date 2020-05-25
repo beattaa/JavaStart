2f1cb914ea954dc6709e6f27b7401d2db58db2d5
beata <beata_sadek@o2.pl>
2020-05-25 10:23:44 +0200

    zajecia 14 - zadanie 3: Schemat blokowy na kod

---
+++ src/zajecia14/zadanie3/SchematBlokowy.java
@@ -0,0 +1,21 @@
+package zajecia14.zadanie3;
+
+import java.util.Scanner;
+
+//Program sumuje liczby, ktorych kwadraty sa mniejsze od 100, a nastepnie bierze z tej sumy reszte z dzielenia przez 2
+public class SchematBlokowy {
+    public static void main(String[] args) {
+        int x = 0;
+        int sum = 0;
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Podaj x= ");
+        x = scanner.nextInt();
+        while (Math.pow(x, 2) < 100) {
+            sum += x;
+            System.out.print("Podaj x= ");
+            x = scanner.nextInt();
+        }
+        sum = sum % 2;
+        System.out.println("Sum= " + sum);
+    }
+}
